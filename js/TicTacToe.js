537a76799bdebeefcf9e4bf0232417b5a4406094
charaf marghin <developermarghin@gmail.com>
2022-09-20 16:33:16 +0100

    add saving functionality and fix prettier

--- js/TicTacToe.js
+++ js/TicTacToe.js
@@ -1,11 +1,11 @@
 class TicTacToe {
   constructor(player1, player2) {
     this.player1 = {
-      name: player1,
+      name: player1.toLowerCase(),
       label: "X",
     };
     this.player2 = {
-      name: player2,
+      name: player2.toLowerCase(),
       label: "O",
     };
     this.currentPlayer = this.player1.label;
@@ -59,9 +59,7 @@ class TicTacToe {
     resetButton.textContent = "Play again";
     resetButton.addEventListener("click", this.resetGame.bind(this));
     const winner = document.createElement("h2");
-    winner.textContent = this.winner
-      ? `the winner is ${this.winner}`
-      : "Tie :) Good game";
+    winner.textContent = this.winner ? `the winner is ${this.winner}` : "Tie :) Good game";
 
     result.appendChild(winner);
     result.appendChild(resetButton);
@@ -91,13 +89,11 @@ class TicTacToe {
     if (this.winner || this.checkForTie()) {
       this.container.innerHTML = "";
       this.container.appendChild(this.renderResult());
+      this.saveGame();
       return;
     }
 
-    this.currentPlayer =
-      this.currentPlayer === this.player1.label
-        ? this.player2.label
-        : this.player1.label;
+    this.currentPlayer = this.currentPlayer === this.player1.label ? this.player2.label : this.player1.label;
   }
 
   /**
@@ -166,19 +162,23 @@ class TicTacToe {
       }
 
       if (cellAValue === cellBValue && cellBValue === cellCValue) {
-        this.winner =
-          this.player1.label === cellAValue
-            ? this.player1.name
-            : this.player2.name;
+        this.winner = this.player1.label === cellAValue ? this.player1.name : this.player2.name;
         this.gameOver = true;
       }
     });
   }
 
+  /**
+   * checks if the board is full or not
+   * @returns {boolean} true if board is full, false if not
+   */
   checkForTie() {
     return this.board.every((e) => e.every((n) => n !== ""));
   }
 
+  /**
+   * @returns {void} resets the board to the initial value
+   */
   resetGame() {
     this.board = [
       ["", "", ""],
@@ -190,6 +190,61 @@ class TicTacToe {
 
     this.container.innerHTML = "";
     this.container.appendChild(this.render());
+  }
+
+  saveGame() {
+    let game = JSON.parse(localStorage.getItem("status")) || {};
+    if(this.winner) {
+      const looser = this.winner === this.player1.name ? this.player2.name : this.player1.name;
+      
+      // winner data
+      if (game[this.winner]) {
+        game[this.winner].wins++;
+        game[this.winner].games++;
+      } else {
+        game[this.winner] = {
+          wins: 1,
+          lost: 0,
+          games: 1,
+        };
+      }
+  
+      // looser data
+      if (game[looser]) {
+        game[looser].lost++;
+        game[this.winner].games++;
+      } else {
+        game[looser] = {
+          wins: 0,
+          lost: 1,
+          games: 1,
+        };
+      }
+    } else {
+      // add game for player1
+      if(game[this.player1.name]) {
+        game[this.player1.name].games++
+      } else {
+        game[this.player1.name] = {
+          wins: 0,
+          lost: 0,
+          games: 1
+        }
+      }
+
+      // add game for player2
+      if(game[this.player2.name]) {
+        game[this.player2.name].games++
+      } else {
+        game[this.player2.name] = {
+          wins: 0,
+          lost: 0,
+          games: 1
+        }
+      }
+    }
+    console.log(game);
+    localStorage.setItem("status", JSON.stringify(game));
   }
 }
 
