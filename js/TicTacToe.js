ac79eda3677284eb484ef0b7bf81817e8bd10614
charaf marghin <developermarghin@gmail.com>
2022-09-20 11:58:41 +0100

    Add winning funcionality

---
+++ js/TicTacToe.js
@@ -0,0 +1,167 @@
+class TicTacToe {
+  constructor() {
+    this.player1 = "X";
+    this.player2 = "O";
+    this.currentPlayer = this.player1;
+    this.board = [
+      ["", "", ""],
+      ["", "", ""],
+      ["", "", ""],
+    ];
+
+    this.winner = null;
+    this.gameOver = false;
+
+    this.container = document.getElementById("root");
+
+    this.container.appendChild(this.render())
+  }
+
+  /**
+   *
+   * @return {HTMLDivElement} the board element
+   */
+  render() {
+    const gameBoard = document.createElement("div");
+    gameBoard.classList.add("board");
+
+    this.board.forEach((row, rowIndex) => {
+      row.forEach((cell, cellIndex) => {
+        const boardCell = document.createElement("div");
+        boardCell.classList.add("board-cell");
+        boardCell.dataset.rowIndex = rowIndex;
+        boardCell.dataset.cellIndex = cellIndex;
+        boardCell.addEventListener("click", this.handleClick.bind(this), {
+          once: true,
+        });
+
+        gameBoard.appendChild(boardCell);
+      });
+    });
+
+    return gameBoard;
+  }
+
+  /**
+   *
+   * @returns {HTMLDivElement} the result modal
+   */
+  renderResult() {
+    const result = document.createElement("div");
+    result.classList.add("result");
+    const resetButton = document.createElement("button");
+    resetButton.textContent = "Play again";
+    const winner = document.createElement("h2");
+    winner.textContent = this.winner
+      ? `the winner is ${this.winner}`
+      : "Tie :) Good game";
+
+    result.appendChild(winner);
+    result.appendChild(resetButton);
+
+    return result;
+  }
+
+  /**
+   *
+   * @param {e} event
+   * @returns void
+   */
+  handleClick(e) {
+    if (this.gameOver || e.target.dataset.selected) {
+      return;
+    }
+
+    e.target.dataset.selected = true;
+    e.target.textContent = this.currentPlayer;
+
+    const rowIndex = e.target.dataset.rowIndex;
+    const cellIndex = e.target.dataset.cellIndex;
+
+    this.board[rowIndex][cellIndex] = this.currentPlayer;
+
+    this.checkForWinner()
+    if(this.winner) {
+      this.container.innerHTML = "";
+      this.container.appendChild(this.renderResult());
+      return;
+    }
+
+    this.currentPlayer =
+      this.currentPlayer === this.player1 ? this.player2 : this.player1;
+  }
+
+  /**
+   *
+   * @returns {boolean} true if there is a winner
+   */
+  checkForWinner() {
+    const winningCombinations = [
+      // Horizontal
+      [
+        [0, 0],
+        [0, 1],
+        [0, 2],
+      ],
+      [
+        [1, 0],
+        [1, 1],
+        [1, 2],
+      ],
+      [
+        [2, 0],
+        [2, 1],
+        [2, 2],
+      ],
+      // Vertical
+      [
+        [0, 0],
+        [1, 0],
+        [2, 0],
+      ],
+      [
+        [0, 1],
+        [1, 1],
+        [2, 1],
+      ],
+      [
+        [0, 2],
+        [1, 2],
+        [2, 2],
+      ],
+      // Diagonal
+      [
+        [0, 0],
+        [1, 1],
+        [2, 2],
+      ],
+      [
+        [0, 2],
+        [1, 1],
+        [2, 0],
+      ],
+    ];
+
+    winningCombinations.forEach((combination) => {
+      const [a, b, c] = combination;
+      const [rowA, cellA] = a;
+      const [rowB, cellB] = b;
+      const [rowC, cellC] = c;
+
+      const cellAValue = this.board[rowA][cellA];
+      const cellBValue = this.board[rowB][cellB];
+      const cellCValue = this.board[rowC][cellC];
+
+      if(!(cellAValue && cellBValue && cellCValue)) {
+        return;
+      }
+
+      if (cellAValue === cellBValue && cellBValue === cellCValue) {
+        this.winner = cellAValue;
+        this.gameOver = true;
+      }
+    });
+  }
+}
+
+export default TicTacToe;
